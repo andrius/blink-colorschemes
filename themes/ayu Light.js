9ec4e00ae4975e4a7100d0dda348145f4c62ca34
Andrius Kairiukstis <k@andrius.mobi>
2021-01-08 14:24:09 +0100

    Update ayu Light.js

--- themes/ayu Light.js
+++ themes/ayu Light.js
@@ -1,4 +1,4 @@
-term_.prefs_.set("background-color", "#fafafa");
-term_.prefs_.set("foreground-color", "#6c7680");
-term_.prefs_.set("cursor-color", "rgba(255,153,64, 0.5)"); /* #ff9940 */
-term_.prefs_.set("color-palette-overrides", ["#000000", "#f07171", "#86b300", "#f2ae49", "#399ee6", "#a37acc", "#4cbf99", "#c7c7c7", "#686868", "#f07171", "#86b300", "#f2ae49", "#399ee6", "#a37acc", "#4cbf99", "#d1d1d1"]);
+t_.prefs_.set("background-color", "#fafafa");
+t_.prefs_.set("foreground-color", "#6c7680");
+t_.prefs_.set("cursor-color", "rgba(255,153,64, 0.5)"); /* #ff9940 */
+t_.prefs_.set("color-palette-overrides", ["#000000", "#f07171", "#86b300", "#f2ae49", "#399ee6", "#a37acc", "#4cbf99", "#c7c7c7", "#686868", "#f07171", "#86b300", "#f2ae49", "#399ee6", "#a37acc", "#4cbf99", "#d1d1d1"]);
